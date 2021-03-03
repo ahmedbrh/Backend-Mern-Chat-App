02ec25ebb8244a6e315f7b431a9bba255ccad45c
ahmedbrh <brhiliahmed@gmail.com>
2021-03-03 01:15:04 +0100

    testing API messeages and handling errors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const env = require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-const conversation = require("./models/schema");
+const mongoData = require("./models/schema");
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +16,63 @@ app.get("/", (req, res) => {
   res.status(200).send("hello ahmed test express ! ");
 });
 
+//post request
+app.post("/new/conversation", (req, res) => {
+  const dataMsg = req.body;
+
+  mongoData.create(dataMsg, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+      console.log(data);
+    }
+  });
+});
+
+//
+
+app.post("/new/message", (req, res) => {
+  mongoData.update(
+    {
+      _id: req.query.id,
+    },
+    { $push: { conversation: req.body } },
+    (err, data) => {
+      if (err) {
+        console.log("error saving the message");
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+    }
+  );
+});
+
+app.get("/get/conversationList", (req, res) => {
+  mongoData.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      data.sort((b, a) => {
+        return a.timestamp - b.timestamp;
+      });
+      let conversations = [];
+
+      data.map(conversationData => {
+        const conversationInfo = {
+          id: conversationData._id,
+          name: conversationData.chatName,
+          timestamp: conversationData.conversation[0].timestamp,
+        };
+
+        conversation.push(conversationInfo);
+      });
+      res.status(200).send(conversations);
+    }
+  });
+});
 // mongodb connection
 
 mongoose.connect(
